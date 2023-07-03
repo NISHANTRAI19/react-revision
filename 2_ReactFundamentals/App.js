4b0740aee9deccb4bb340e8b0db272e7aa261ccf
Nishant Rai <rai7nishant@gmail.com>
2023-07-03 11:30:45 +0530

    day 3

--- 2_ReactFundamentals/App.js
+++ 2_ReactFundamentals/App.js
@@ -1,26 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-    "h1",
-    { id: "blahblah" },
-    "Hellowwww world from React"
-);
+const heading = <h1 className="head">HI from JSX</h1>
 
-const nested = React.createElement("div", { id: "nested-parent" }, [
-    React.createElement("div", { id: "nested-child1" }, [
-        React.createElement("h1", { id: "1" }, "H1111111111"),
-        React.createElement("h1", { id: "2" }, "H22222222222"),
-    ]),
-    React.createElement("div", { id: "nested-child2" }, [
-        React.createElement("h1", { id: "1" }, "H1111111111"),
-        React.createElement("h1", { id: "2" }, "H22222222222"),
-    ]),
-]);
 
-console.log(heading);
-setTimeout(() => {
-    // code to check dynamic dom manipulation
-    const root = ReactDOM.createRoot(document.getElementById("reactroot")); // creating react root from ID reactroot
-    root.render(nested); //nested or heading                                   //converting js object i.e nested or heading to html
-    // this is very messy so we use JSx
-}, 3000);
+
+const HeadingComponent = () => <h1>FROM FUNCTIONAL </h1>
+
+const root = ReactDOM.createRoot(document.getElementById("reactroot"));
+root.render(<HeadingComponent />);
