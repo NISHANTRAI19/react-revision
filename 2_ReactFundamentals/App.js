f92e07b4a818205687788f489eea575d9a037666
Nishant Rai <rai7nishant@gmail.com>
2023-07-03 20:07:16 +0530

    notes completed

--- 2_ReactFundamentals/App.js
+++ 2_ReactFundamentals/App.js
@@ -3,8 +3,14 @@ import ReactDOM from "react-dom/client";
 const heading = <h1 className="head">HI from JSX</h1>
 
 
+const title = (<h1>rendering JSX</h1>)
+function HeadingComponent() {
+    return (
+        <h1>FROM FUNCTIONAL </h1>
+    )
 
-const HeadingComponent = () => <h1>FROM FUNCTIONAL </h1>
+} //HeadingComponent = () => <h1>FROM FUNCTIONAL </h1>
 
 const root = ReactDOM.createRoot(document.getElementById("reactroot"));
 root.render(<HeadingComponent />);
+//root.render(title);
